e110f4e2df2d01a2d960e9f2bcffe73e25bc228d
Lewis McNeill <lewisfmcn@gmail.com>
2020-12-05 18:58:26 +0000

    completed day 5

---
+++ day_5/day_5.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Day5
+  class Task1
+    def self.run
+      tickets.max
+    end
+
+    def self.tickets
+      input_array = File.readlines("input.txt").map(&:strip).map { |element| element.split("") }
+      input_array.map do |ticket|
+        row = ticket[0..6].each_with_object({ "max": 127, "min": 0 }) do |pos, product|
+          middle = (product[:max] + product[:min]) / 2
+          pos == "F" ? product[:max] = middle : product[:min] = (middle + 1)
+        end
+
+        column = ticket[7..9].each_with_object({ "max": 7, "min": 0 }) do |pos, product|
+          middle = (product[:max] + product[:min]) / 2
+          pos == "L" ? product[:max] = middle : product[:min] = middle + 1
+        end
+
+        (row[:max] * 8) + column[:max]
+      end
+    end
+  end
+
+  class Task2
+    def self.run
+      tickets = Day5::Task1.tickets.sort
+
+      tickets.reduce(tickets.first - 1) do |product, ticket|
+        return product + 1 if product + 1 != ticket
+
+        product = ticket
+        product
+      end
+    end
+  end
+end
