09253b53980023c4248848dad30b2f4da981d379
Lewis McNeill <lewisfmcn@gmail.com>
2020-12-15 17:49:04 +0000

    completed day 14 task 2

--- lib/day_14/day_14.rb
+++ lib/day_14/day_14.rb
@@ -16,16 +16,16 @@ class Day14
   end
 
   def task_1
-    execute_action([], [], 0).sum
+    execute_action({}, [], 0).sum
   end
 
   def task_2
+    pp execute_float_action({}, [], 0).values.sum
   end
 
   private
 
   def execute_action(memory, mask, line)
-    pp line
     return memory.compact if line >= @input_array.length
 
     command = @input_array[line]
@@ -39,5 +39,41 @@ class Day14
     end
 
     execute_action(memory, mask, line + 1)
+  end
+
+  def execute_float_action(memory, mask, line)
+    return memory.compact if line >= @input_array.length
+
+    command = @input_array[line]
+
+    if command[:action] == :mask
+      mask = command[:value].split("")
+    else
+      generate_values(command[:position].to_s(2).rjust(36, "0").split(""), mask)
+        .each { |value| memory[value] = command[:value].to_i(2) }
+    end
+
+    execute_float_action(memory, mask, line + 1)
+  end
+
+  def generate_values(input, mask)
+    base_input = input.zip(mask).map do |input_value, mask_value|
+      next "X" if mask_value == "X"
+
+      mask_value == "1" ? "1" : input_value
+    end
+
+    generate_variety([], base_input)
+  end
+
+  def generate_variety(variety, input)
+    return variety << input.join("").to_i(2) if input.count("X").zero?
+
+    position = input.find_index { |ele| ele == "X" }
+
+    input[position] = "0"
+    generate_variety(variety, input.dup)
+    input[position] = "1"
+    generate_variety(variety, input.dup)
   end
 end
