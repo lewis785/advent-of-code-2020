7e4b5c58585c23fa890053347085ef57870f9966
Lewis McNeill <lewisfmcn@gmail.com>
2020-12-15 20:00:18 +0000

    completed day15 task 2

--- lib/day_15/day_15.rb
+++ lib/day_15/day_15.rb
@@ -9,30 +9,40 @@ class Day15
 
   def task_1
     inputs = @input_array.map do |line|
-      line.each_with_index.map { |ele, index| [ele.to_i, index] }.to_h
+      line.each_with_index.map { |ele, index| [ele.to_i, index + 1] }.to_h
     end
 
-    inputs.map { |input| find_2020_in_sequence(input, input.length, 0) }
-
-    # pp find_2020_in_sequence(inputs.first, 3, 0)
+    inputs.map { |input| find_value_in_turn(2020, input, input.length + 1, 0)[:value] }
   end
 
   def task_2
+    inputs = @input_array.map do |line|
+      line.each_with_index.map { |ele, index| [ele.to_i, index + 1] }.to_h
+    end
 
+    loops = 30000000 / 10000
+    inputs.map do |input|
+      result = { last_occurrences: input, turn: input.length + 1, value: 0 }
+
+      loops.times do |x|
+        result = find_value_in_turn((x + 1) * 10000, result[:last_occurrences], result[:turn], result[:value])
+      end
+      result[:value]
+    end
   end
 
   private
 
-  def find_2020_in_sequence(last_occurrences, turn, value)
-    return value if turn + 1 == 2020
+  def find_value_in_turn(target_turn, last_occurrences, turn, value)
+    return { last_occurrences: last_occurrences, turn: turn, value: value } if turn == target_turn
 
     if last_occurrences.key? value
       last_occurred = last_occurrences[value]
       last_occurrences[value] = turn
-      find_2020_in_sequence(last_occurrences, turn + 1, turn - last_occurred)
+      find_value_in_turn(target_turn, last_occurrences, turn + 1, turn - last_occurred)
     else
       last_occurrences[value] = turn
-      find_2020_in_sequence(last_occurrences, turn + 1, 0)
+      find_value_in_turn(target_turn, last_occurrences, turn + 1, 0)
     end
   end
 
